a5a33e22e19a2ce80c1b7006c1c2111e6824e297
LuisMi <luis.miguel.almaraz@ericsson.com>
2018-09-27 09:13:50 +0200

    First Rmarkdown draft

--- StackOverflowAnalysis.R
+++ StackOverflowAnalysis.R
@@ -16,7 +16,8 @@ View(survey_results_public)
 
 # First thing to do is to explore the data, what is in it, if it is clean/complete, etc
 
-
+library(dplyr)
+library(tidyr)
 
 # Then we can look into ideas what "question go answer" by looking in Kaggle some Kernels
 # https://www.kaggle.com/stackoverflow/so-survey-2017/kernels
@@ -25,3 +26,11 @@ View(survey_results_public)
 
 # I was curious to practice a bar chart to see # of contributions per country
 library(ggplot2)
+
+# Get mean salary per country and number of respondents
+
+mean_salary= survey_results_public %>% group_by(Country) %>% summarise(mean=mean(Salary,na.rm=TRUE),number=n())
+
+# Get top 10 respondent countries
+
+top_mean_salary = top_n (mean_salary, 10)
